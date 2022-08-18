2844bd0a0395e1d3b5231a3bdd7b1cf6e1195f5e
shivaniahirwar <shivaniahirwar090@gmail.com>
2022-08-18 16:37:32 +0530

    committed...

---
+++ SampleFrameWork/src/test/java/Practice/SampleTest.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class SampleTest 
+{
+    public static void main(String[] args)
+    {
+    	System.out.println("jenkins.......practice");
+		System.out.println("project..........");
+		System.out.println("pratice is must to improve ourself");	
+		System.out.println("details of the jenkins");
+	}
+}
